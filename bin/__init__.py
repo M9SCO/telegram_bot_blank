95a390aa18ca6682b7f42ae07ba945cade67f5f7
M9SCO <48180788+M9SCO@users.noreply.github.com>
2020-07-26 22:33:50 +0600

    migrate sql module to bin and default init him

--- bin/__init__.py
+++ bin/__init__.py
@@ -2,7 +2,8 @@ from telegram import Bot
 from telegram.ext import Updater
 
 from .config import token
-
+from .sql import connection_to_database
 
 updater: Updater = Updater(token=token, use_context=True)
 bot: Bot = updater.bot
+cnx, cursor = connection_to_database()
